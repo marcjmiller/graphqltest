b237633aa16875dccae03553db4b95985ab9601e
Marc Miller <millermj@dgs1sdt.com>
2020-10-02 08:06:01 -0400

    Initial commit with skeleton Springboot app

---
+++ src/test/java/com/qraphqltest/graphqltest/GraphqltestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qraphqltest.graphqltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqltestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
